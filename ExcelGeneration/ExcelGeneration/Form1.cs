12c96f4aa227c0a93d9883c8c5e19554b06e1ffa
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-11-28 19:51:11 +0100

    Reference to Excel

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ExcelGeneration.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelGeneration
 {
